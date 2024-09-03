25871a072559818486fe70201a9ccab32be06095
rohan-kapoor <kapoorrohan2004@gmail.com>
2024-09-03 15:53:50 +0530

    Debugging

--- binary_tree.h
+++ binary_tree.h
@@ -3,7 +3,7 @@
 
 #include "tree_operations.h"
 
-typedef struct TreeNode TreeNode;
+// typedef struct TreeNode TreeNode;
 
 TreeNode* insert(TreeNode* root, int data);
 TreeNode* delete_node(TreeNode* root, int data);
