9b0476af7db695c6b20692aab1476a14cf613af1
rohan-kapoor <kapoorrohan2004@gmail.com>
2024-09-03 23:48:36 +0530

    Formatting BST files

--- binary_search_tree.h
+++ binary_search_tree.h
@@ -1,11 +1,6 @@
-#ifndef Binary_search_tree
-#define Binary_search_tree
-
 #include "tree_operations.h"
 
 // Function prototypes
-TreeNode* insert(TreeNode* root, int key);
-TreeNode* search(TreeNode* root, int key);
-TreeNode* delete_node(TreeNode* root, int key);
-
-#endif
+TreeNode* insert_BST(TreeNode* root, int key);
+TreeNode* search_BST(TreeNode* root, int key);
+TreeNode* delete_node_BST(TreeNode* root, int key);
